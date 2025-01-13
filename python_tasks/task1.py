982e37292dcf4c9fa1a7eb1a0a4cce92b3357e54
Rami Ferjani <ramiferjani.20@gmail.com>
2025-01-13 21:12:59 +0000

    Add files via upload

--- python_tasks/task1.py
+++ python_tasks/task1.py
@@ -1,7 +1,30 @@
-if __name__ == '__main__':
-    n = int(input()) 
+# --------------------
+# -- Numpy => Intro --
+# --------------------
+# - NumPy Is A Python Third-Party Module To Deal With Arrays & Matrices
+# - NumPy Stand For Numerical Python
+# - NumPy Is Open Source
+# - NumPy Support Dealing With Large Multidimensional Arrays & Matrices
+# - NumPy Has Many Mathematical Functions To Deal With This Elements
+# ------------------------------------------------------------
+# * Why We Use NumPy Array
+# - Consume Less Memory
+# - Very Fast Compared To Python List
+# - Easy To Use
+# - Support Element Wise Operation
+# - Elements Are Stored Contiguous
+# -------------------------------------------
+# * Python Lists
+# ---- Homogeneous => Can Contains The Same Type of Objects
+# ---- Heterogeneous => Can Contains Different Types of Objects.
+# --------------------------------------------------------------
+# - The Items in The Array Have to Be Of The Same Type
+# - You Can Be Sure Whats The Storage Size Needed for The Array
+# - NumPy Arrays Are Indexed From 0
+# --------------------------------------------------------------
+# - NumPy On Github => https://github.com/numpy/numpy
+# ---------------------------------------------------
 
-    for i in range(n) : 
-        print( i**2 )
+import numpy as np
 
-#The provided code stub reads and integer, n  , from STDIN. For all non-negative integers i< n, print(i**2) .
+print(np.__version__)
